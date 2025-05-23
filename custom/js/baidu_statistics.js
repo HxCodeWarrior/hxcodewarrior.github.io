10ca63eb7abe22c38779e5d5cfd6ec1ef0dd0734
HxCodeWarrior <296946361@qq.com>
2025-05-23 13:41:16 +0800

    使用自己的域名作为网站数据统计

--- custom/js/baidu_statistics.js
+++ custom/js/baidu_statistics.js
@@ -2,7 +2,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ae2f6cce94e44dbe678d8be347160714";
+  hm.src = "https://hm.baidu.com/hm.js?4354e1e2764c5319fb90e264e164b29f";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
