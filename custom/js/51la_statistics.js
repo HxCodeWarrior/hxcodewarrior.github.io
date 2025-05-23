b76f4c0f53d9477a115084117ab87879212ad412
HxCodeWarrior <296946361@qq.com>
2025-05-23 13:49:58 +0800

    修复51la统计代码安装错误的问题

--- custom/js/51la_statistics.js
+++ custom/js/51la_statistics.js
@@ -1,15 +1,6 @@
-// 51la统计脚本
+// 使用DOM API创建和插入脚本元素，替代已弃用的document.write
 (function() {
-    // 创建并加载51la统计脚本
-    var la_script = document.createElement('script');
-    la_script.charset = "UTF-8";
-    la_script.id = "LA_COLLECT";
-    la_script.src = "//sdk.51.la/js-sdk-pro.min.js";
-    document.head.appendChild(la_script);
-    
-    // 初始化51la统计
-    la_script.onload = function() {
-      LA.init({id:"3MFYKYISU9qVaNEC",ck:"3MFYKYISU9qVaNEC",autoTrack:true});
-    };
-  }
-)();
+    const script = document.createElement('script');
+    script.textContent = `!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3MFYKYISU9qVaNEC",ck:"3MFYKYISU9qVaNEC",autoTrack:true});`;
+    document.head.appendChild(script);
+})();
