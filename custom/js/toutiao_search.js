8e12746ca48af739f12849bae621d9eb408b1689
HxCodeWarrior <296946361@qq.com>
2025-05-31 14:28:32 +0800

    修复搜狗搜索、神马搜索无法验证网站的问题

---
+++ custom/js/toutiao_search.js
@@ -0,0 +1,7 @@
+(function(){
+var el = document.createElement("script");
+el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?72a793b09c3ec52d0fd59024cc168f703070c53bcd41da2611585912ac5baad4bc434964556b7d7129e9b750ed197d397efd7b0c6c715c1701396e1af40cec962b8d7c8c6655c9b00211740aa8a98e2e";
+el.id = "ttzz";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(el, s);
+})(window)
